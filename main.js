02d8d202a9dac7a836be899ade7bfe9984a1503e
kiyo <pisutachio.48@gmail.com>
2025-11-01 12:43:56 +0900

    saisinn

--- main.js
+++ main.js
@@ -15,13 +15,17 @@
   const my = document.querySelector('#my');
   const shouri = document.querySelector('#shouri');
   const repeat = document.querySelector('#repeat');
+  const winbw = document.querySelector('#winbw');
+  const losbw = document.querySelector('#losbw');
+  const gamehantei = document.querySelector('#gamehantei');
 
   function buttonStop() {
     gu.disabled = true;
     tyoki.disabled = true;
     pa.disabled = true;
   }
-
+  let victory = 0;
+  let loss = 0;
   function judge(myhandIndex, comhand) {
     if (myhandIndex === comhand) {
       return 'あいこ'
@@ -32,10 +36,13 @@
       (myhandIndex === 2 && comhand === 0);
 
     if (win) {
+      victory++
       return '勝ち';
     }
+    loss++
+    return '負け';
 
-    return '負け'
+
   }
   function playbutton(myhandIndex) {
     comhand = Math.floor(Math.random() * gutyokipa.length);
@@ -46,9 +53,41 @@
     buttonStop();
     repeat.style.visibility = 'visible'
       ;
+    winbw.textContent = `勝数 ${victory} 回`;
+    losbw.textContent = `負数 ${loss} 回`;
+    gameover();
   }
 
-  
+  function gameover() {
+    if (victory === 3) {
+      
+      setTimeout(() => {
+        hyoudai.style.display='none';
+    title.style.display='block';
+        game.style.display = 'none'
+        // start.style.display = 'none';
+        gamehantei.style.display = 'block';
+        gamehantei.textContent = 'YOU WIN!';
+        title.style.backgroundColor='gold';
+      }, 1500);
+
+
+    } else if
+      (loss === 3) {
+      setTimeout(() => {
+        hyoudai.style.display='none';
+    title.style.display='block';
+        game.style.display = 'none'
+        // start.style.display = 'none';
+        gamehantei.style.display = 'block';
+        gamehantei.textContent = 'YOU LOSE!';
+        title.style.backgroundColor='gray';
+      }, 1500);
+    }
+    
+  }
+
+
   let comhand;
 
   start.addEventListener('click', () => {
@@ -56,6 +95,20 @@
     hyoudai.style.display = 'block';
     game.style.display = 'block';
     repeat.style.visibility = 'hidden'
+     com.style.animation = 'janken-loop 0.6s infinite';
+    shouri.textContent = '';
+     victory = 0;
+   loss = 0;
+    winbw.textContent = `勝数 ${victory} 回`;
+    losbw.textContent = `負数 ${loss} 回`;
+    gamehantei.style.display = 'none';
+    shouri.textContent = '';
+
+    gu.disabled = false;
+    tyoki.disabled = false;
+    pa.disabled = false;
+    repeat.style.visibility = 'hidden'
+    title.style.backgroundColor = '';
   });
 
   gu.addEventListener('click', () => playbutton(0));
@@ -65,6 +118,7 @@
   repeat.addEventListener('click', () => {
 
     com.style.animation = 'janken-loop 0.6s infinite';
+    shouri.textContent = '';
 
     gu.disabled = false;
     tyoki.disabled = false;
